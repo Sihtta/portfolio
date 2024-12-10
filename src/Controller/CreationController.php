854b41c4fca539ef7b2ef65aa5300b6102d7f0db
Théo <theo.schaller57@gmail.com>
2024-12-10 08:12:34 +0100

    🚧 Creation Controller

---
+++ src/Controller/CreationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CreationRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreationController extends AbstractController
+{
+    #[Route("/", "creation.index", methods: ["GET"])]
+    public function index(CreationRepository $creationRepository): Response
+    {
+        $creations = $creationRepository->findAll();
+
+        return $this->render('pages/creation.html.twig', [
+            'creations' => $creations,
+        ]);
+    }
+}
+
