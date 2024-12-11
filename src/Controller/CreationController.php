e59a90308bd215752a89e4552de7a389e9b1b610
Théo <theo.schaller57@gmail.com>
2024-12-11 11:28:25 +0100

    🚧 Améliorations affichages des créations

--- src/Controller/CreationController.php
+++ src/Controller/CreationController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Repository\CreationRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CreationController extends AbstractController
 {
