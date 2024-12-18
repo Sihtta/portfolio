3022e8da2a002070c8720b88028a3fab1aa8c56c
Matthis <147597642+Sihtta@users.noreply.github.com>
2024-12-18 11:44:36 +0100

    Formulaire fonctionnelle + pré remplissage champs

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,15 +6,40 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
-    {
+    #[Route('/contact', name: 'contact.index')]
+    public function index(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response {
         $contact = new Contact();
+
+        if ($this->getUser()) {
+            $contact->setFullName($this->getUser()->getFullName())
+                ->setEmail($this->getUser()->getEmail());
+        }
         $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $manager->persist($contact);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre demande a été envoyé avec succès.'
+            );
+
+            return $this->redirectToRoute('contact.index');
+        }
 
         return $this->render('pages/contact/index.html.twig', [
             'form' => $form->createView(),
